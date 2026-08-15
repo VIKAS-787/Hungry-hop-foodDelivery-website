e41269b54d9cf6d7aac3e6fa069281850bcd168e
VIKAS-787 <>
2026-08-15 14:50:53 +0530

    change in the app.js

--- Server/app.js
+++ Server/app.js
@@ -1,39 +1,39 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import connectDB from "./Config/Db.js";
 import foodRouter from "./router/food-router.js";
 import userRouter from "./router/user-router.js";
 import cartRouter from "./router/cart-router.js";
 import orderRouter from "./router/order-router.js";
 
-import dotenv from "dotenv";
-import path from "path";
+dotenv.config();
 
-dotenv.config({ path: path.resolve("./.env") });
-console.log("KEY:", process.env.TESTAPI_KEY);
-//database connection
+// Database connection
 connectDB();
 
-//app config
+// App config
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-//middlewares
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// api routes
-app.use("/api/food",foodRouter);
+// API routes
+app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order",orderRouter)
+app.use("/api/order", orderRouter);
 
-app.get("/",(req,res)=>{
-    res.send("server is running");
-})
+// Test route
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
-
-app.listen(PORT,()=>{
- console.log(`Server running on http://localhost:${PORT}`);
-})
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
