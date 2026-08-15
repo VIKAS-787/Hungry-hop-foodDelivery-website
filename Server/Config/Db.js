fd820a892d2162131ba0e4a70118bf0f0e3372c7
VIKAS-787 <>
2026-08-15 15:14:21 +0530

    adding the dataconection

--- Server/Config/Db.js
+++ Server/Config/Db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected");
   } catch (error) {
-    console.log("Error in connecting to database", error);
+    console.error("Error in connecting to database:", error.message);
+    process.exit(1);
   }
 };
 
